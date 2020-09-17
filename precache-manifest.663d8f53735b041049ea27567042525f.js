6f756d8a6b7df96ae9cdcca8f069ddf6f70592f1
Ming Chen <mzchen14@gmail.com>
2020-09-17 19:51:42 -0400

    Updates

---
+++ precache-manifest.663d8f53735b041049ea27567042525f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92e11c80afd81caad18e4401b0a7411",
+    "url": "/algolia-movies/index.html"
+  },
+  {
+    "revision": "576d4137ba2d8961937a",
+    "url": "/algolia-movies/static/css/2.9e82207c.chunk.css"
+  },
+  {
+    "revision": "c2949a87e794b407a761",
+    "url": "/algolia-movies/static/css/main.cbc240d8.chunk.css"
+  },
+  {
+    "revision": "576d4137ba2d8961937a",
+    "url": "/algolia-movies/static/js/2.e9730be7.chunk.js"
+  },
+  {
+    "revision": "e36027de53b58e5552082a7fddfcd859",
+    "url": "/algolia-movies/static/js/2.e9730be7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2949a87e794b407a761",
+    "url": "/algolia-movies/static/js/main.d703e898.chunk.js"
+  },
+  {
+    "revision": "9160d7220402aa8ec217",
+    "url": "/algolia-movies/static/js/runtime-main.d2034627.js"
+  }
+]);
